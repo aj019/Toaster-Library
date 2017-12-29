62c29da4cbf87ed56da163e8ae0cad08ce805fb8
Anuj Gupta <junagupta19@gmail.com>
2017-12-29 20:43:28 +0530

    Initial commit

--- toasterlibrary/src/main/java/in/androidmate/anujgupta/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/in/androidmate/anujgupta/toasterlibrary/ToasterMessage.java
@@ -9,7 +9,7 @@ import android.widget.Toast;
 
 public class ToasterMessage {
 
-    public void s(Context c,String message){
+    public static void s(Context c,String message){
 
         Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
 
